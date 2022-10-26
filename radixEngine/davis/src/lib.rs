b27ab83bc3b0a7dc5940a88d65db624b71c5c372
Eric Huang <erhyik0122@gmail.com>
2022-10-25 19:41:10 -0700

    [Scrypto] Create AggieSwap policy

--- radixEngine/davis/src/lib.rs
+++ radixEngine/davis/src/lib.rs
@@ -1,36 +1,65 @@
 use scrypto::prelude::*;
 
 blueprint! {
-    struct Hello {
-        // Define what resources and data will be managed by Hello components
-        sample_vault: Vault
+    struct Davis {
+        // Define what resources and data will be managed by Davis components
+        aggieSwap: Vault,
+        xrd: Vault,
+        price: Decimal,
+        transactionFee: Decimal,
     }
 
-    impl Hello {
+    impl Davis {
         // Implement the functions and methods which will manage those resources and data
         
         // This is a function, and can be called directly on the blueprint once deployed
-        pub fn instantiate_hello() -> ComponentAddress {
-            // Create a new token called "HelloToken," with a fixed supply of 1000, and put that supply into a bucket
-            let my_bucket: Bucket = ResourceBuilder::new_fungible()
-                .metadata("name", "HelloToken")
-                .metadata("symbol", "HT")
+        pub fn new(price: Decimal, transactionFee: Decimal) -> ComponentAddress {
+            // Create a new token called "AggieSwap" with a fixed supply of 1000, and put that supply into a bucket
+            let aggieSwapBucket: Bucket = ResourceBuilder::new_fungible()
+                .metadata("name", "AggieSwap")
+                .metadata("symbol", "AS")
                 .initial_supply(1000);
 
-            // Instantiate a Hello component, populating its vault with our supply of 1000 HelloToken
+            // Instantiate a Davis component, populating its vault with our supply of 1000 AggieSwap
             Self {
-                sample_vault: Vault::with_bucket(my_bucket)
+                aggieSwap: Vault::with_bucket(aggieSwapBucket),
+                xrd: Vault::new(RADIX_TOKEN),
+                price: price,
+                transactionFee: transactionFee,
             }
             .instantiate()
             .globalize()
         }
 
-        // This is a method, because it needs a reference to self.  Methods can only be called on components
-        pub fn free_token(&mut self) -> Bucket {
-            info!("My balance is: {} HelloToken. Now giving away a token!", self.sample_vault.amount());
+        // Get the price of AggieSwap
+        pub fn get_price(&mut self) -> Decimal{
+            self.price
+        }
+
+        pub fn get_transaction_fee(&mut self) -> Decimal{
+            self.transactionFee
+        }
+
+
+        // Buy AggieSwap
+        pub fn buy_token(&mut self, mut payment: Bucket, mut fee: Bucket) -> (Bucket, Bucket, Bucket) {
+            
+            assert!(payment.amount() >= self.price, "Payment is not enough for buying at least one token");
+            assert!(fee.amount() >= self.transactionFee, "Transaction fee is not enough for executing the transaction");
+
+            // Calculate how many tokens can the user gets
+            let tokens:Decimal;
+            let cost:Decimal;
+
+            tokens = (payment.amount() / self.price).floor();
+            cost = tokens * self.price;
+            
+            // Take the xrd from bucket to vault
+            self.xrd.put(payment.take(cost));
+            self.xrd.put(fee.take(self.transactionFee));
+
             // If the semi-colon is omitted on the last line, the last value seen is automatically returned
-            // In this case, a bucket containing 1 HelloToken is returned
-            self.sample_vault.take(1)
+            (self.aggieSwap.take(tokens), payment, fee)
         }
     }
 }
