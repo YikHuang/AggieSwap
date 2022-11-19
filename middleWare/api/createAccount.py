3bbce81c04da1f52d368cf76a98fcfb1e54feb87
GitGitTester <ezno84@gmail.com>
2022-11-18 20:52:05 -0800

    json part add

--- middleWare/api/createAccount.py
+++ middleWare/api/createAccount.py
@@ -1,24 +1,29 @@
 import json
 import subprocess
 
+# =============================================================================
+# resim new-account commnad result 
+#
+# A new account has been created!
+# Account component address: account_sim1___________some string___________
+# Public key: ___________some string___________
+# Private Key: ___________some string___________
+# =============================================================================
 
-def main():
+def createAccount():
+    # To make JSON reponse
     result_dict = { 
         "accountAddr" : "",
         "privateKey" : "",
     }
 
-    create_new_acct_result = subprocess.Popen( ["resim", "new-account"], stdout=subprocess.PIPE )
+    create_new_acct_result = subprocess.Popen( 
+            ["resim", "new-account"], 
+            stdout=subprocess.PIPE 
+        )
 
     splited_new_acct_result = str(create_new_acct_result.communicate())
     splited_new_acct_result = splited_new_acct_result.split('\\n')
-
-    # resim new-account commnad result 
-    #
-    # A new account has been created!
-    # Account component address: account_sim1___________some string___________
-    # Public key: ___________some string___________
-    # Private Key: ___________some string___________
 
     accountAddr_value = splited_new_acct_result[1].split(': ')
     accountAddr_value = str(accountAddr_value[1])
@@ -29,11 +34,15 @@ def main():
     result_dict["accountAddr"] = accountAddr_value
     result_dict["privateKey"] = privateKey_value
 
+    # make reponse dictionary
+    result_dict = json.dumps(result_dict)
     result_dict = json.loads(result_dict)
+
+    print(result_dict)
+    print(type(result_dict))
 
     return result_dict
 
 
-
 if __name__ == "__main__":
-    main()
+    createAccount()
